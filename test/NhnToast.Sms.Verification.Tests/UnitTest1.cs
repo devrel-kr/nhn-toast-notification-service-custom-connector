bac2a162cdb1d0deef2b36e0988859c4c0b383ff
Justin Yoo <justin.yoojh@gmail.com>
2022-03-30 12:46:35 +0900

    Add new API proxy for verification

---
+++ test/NhnToast.Sms.Verification.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NhnToast.Sms.Verification.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
