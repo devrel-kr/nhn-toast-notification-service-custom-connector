82eb3925666c8f1f0cc1bd0f125b69c1fc2fc755
Justin Yoo <justin.yoojh@gmail.com>
2022-04-05 10:27:32 +0000

    Add endpoint

--- src/NhnToast.Sms.Verification/Triggers/ListSenders.cs
+++ src/NhnToast.Sms.Verification/Triggers/ListSenders.cs
@@ -1,5 +1,7 @@
+using System;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Http;
@@ -8,10 +10,13 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Extensions;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 using Newtonsoft.Json;
+
+using SmartFormat;
 
 namespace Toast.Sms.Verification.Triggers
 {
@@ -27,23 +32,44 @@ namespace Toast.Sms.Verification.Triggers
         [FunctionName(nameof(ListSenders))]
         [OpenApiOperation(operationId: "Sender.List", tags: new[] { "sender" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "x-functions-key", In = OpenApiSecurityLocationType.Header)]
+        [OpenApiParameter(name: "sendNo", Type = typeof(string), In = ParameterLocation.Query, Required = false, Description = "전송번호")]
+        [OpenApiParameter(name: "useYn", Type = typeof(string), In = ParameterLocation.Query, Required = false, Description = "Use Y/N")]
+        [OpenApiParameter(name: "blockYn", Type = typeof(string), In = ParameterLocation.Query, Required = false, Description = "Block Y/N")]
+        [OpenApiParameter(name: "pageNum", Type = typeof(string), In = ParameterLocation.Query, Required = false, Description = "페이지 번호")]
+        [OpenApiParameter(name: "pageSize", Type = typeof(string), In = ParameterLocation.Query, Required = false, Description = "페이지 크기")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(object), Description = "The OK response")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "GET", Route = "senders")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            var appKey = Environment.GetEnvironmentVariable("Toast__AppKey");
+            var secretKey = Environment.GetEnvironmentVariable("Toast__SecretKey");
+            var baseUrl = Environment.GetEnvironmentVariable("Toast__BaseUrl");
+            var version = Environment.GetEnvironmentVariable("Toast__Version");
+            var endpoint = Environment.GetEnvironmentVariable("Toast__Endpoints__ListSenders");
+            var options = new
+            {
+                version = version,
+                appKey = appKey,
+                sendNo = req.Query["sendNo"].ToString(),
+                useYn = req.Query["useYn"].ToString(),
+                blockYn = req.Query["blockYn"].ToString(),
+                pageNum = req.Query["pageNum"].ToString().IsNullOrWhiteSpace() ? "1" : req.Query["pageNum"].ToString(),
+                pageSize = req.Query["pageSize"].ToString().IsNullOrWhiteSpace() ? "15" : req.Query["pageSize"].ToString()
+            };
+            var requestUrl = Smart.Format($"{baseUrl.TrimEnd('/')}/{endpoint.TrimStart('/')}", options);
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var http = new HttpClient();
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            // Act
+            http.DefaultRequestHeaders.Add("X-Secret-Key", secretKey);
+            var result = await http.GetAsync(requestUrl).ConfigureAwait(false);
 
-            return new OkObjectResult(responseMessage);
+            var payload = await result.Content.ReadAsAsync<object>().ConfigureAwait(false);
+
+
+            return new OkObjectResult(payload);
         }
     }
 }
--- test/NhnToast.Sms.Verification.Tests/Triggers/ListSendersTests.cs
+++ test/NhnToast.Sms.Verification.Tests/Triggers/ListSendersTests.cs
@@ -13,10 +13,10 @@ using WorldDomination.Net.Http;
 namespace Toast.Sms.Verification.Tests.Triggers
 {
     [TestClass]
-    public class ListSenders
+    public class ListSendersTests
     {
         [TestMethod]
-        public async Task Given_Parameters_When_ListSenders_Invoked_Then_It_Should_Return_Result()
+        public async Task Given_Parameters_With_Null_When_ListSenders_Invoked_Then_It_Should_Return_False()
         {
             // Arrange
             var config = new ConfigurationBuilder().AddJsonFile("test.settings.json").Build();
@@ -48,6 +48,43 @@ namespace Toast.Sms.Verification.Tests.Triggers
             // Assert
             result.StatusCode.Should().Be(HttpStatusCode.OK);
             ((bool)payload.header.isSuccessful).Should().BeFalse();
+
+            result.Dispose();
+        }
+
+        [TestMethod]
+        public async Task Given_Parameters_With_Default_When_ListSenders_Invoked_Then_It_Should_Return_True()
+        {
+            // Arrange
+            var config = new ConfigurationBuilder().AddJsonFile("test.settings.json").Build();
+            var appKey = config.GetValue<string>("Toast:AppKey");
+            var secretKey = config.GetValue<string>("Toast:SecretKey");
+            var baseUrl = config.GetValue<string>("Toast:BaseUrl");
+            var version = config.GetValue<string>("Toast:Version");
+            var endpoint = config.GetValue<string>("Toast:Endpoints:ListSenders");
+            var options = new
+            {
+                version = version,
+                appKey = appKey,
+                sendNo = default(string),
+                useYn = default(string),
+                blockYn = default(string),
+                pageNum = 1,
+                pageSize = 15
+            };
+            var requestUrl = Smart.Format($"{baseUrl.TrimEnd('/')}/{endpoint.TrimStart('/')}", options);
+
+            var http = new HttpClient();
+
+            // Act
+            http.DefaultRequestHeaders.Add("X-Secret-Key", secretKey);
+            var result = await http.GetAsync(requestUrl).ConfigureAwait(false);
+
+            dynamic payload = await result.Content.ReadAsAsync<object>().ConfigureAwait(false);
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            ((bool)payload.header.isSuccessful).Should().BeTrue();
 
             result.Dispose();
         }
