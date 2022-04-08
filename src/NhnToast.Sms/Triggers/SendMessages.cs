15a91addeea6563e850a06b191cbe5684a1ee3d3
dunblx05 <dubblx05@naver.com>
2022-04-08 17:11:06 +0900

    Change SendMessages proxy function not use query parameter

--- src/NhnToast.Sms/Triggers/SendMessages.cs
+++ src/NhnToast.Sms/Triggers/SendMessages.cs
@@ -18,6 +18,7 @@ using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
 using SmartFormat;
+using System.Net.Http.Formatting;
 
 namespace Toast.Sms.Triggers
 {
@@ -33,9 +34,7 @@ namespace Toast.Sms.Triggers
         [FunctionName(nameof(SendMessages))]
         [OpenApiOperation(operationId: "Sender.List", tags: new[] { "sender" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "x-functions-key", In = OpenApiSecurityLocationType.Header)]
-        [OpenApiParameter(name: "body", Type = typeof(string), In = ParameterLocation.Query, Required = true, Description = "Body of Message to send")]
-        [OpenApiParameter(name: "sendNo", Type = typeof(string), In = ParameterLocation.Query, Required = true, Description = "Message Sender's No")]
-        [OpenApiParameter(name: "recipientNo", Type = typeof(string), In = ParameterLocation.Query, Required = true, Description = "Message Recipient's No")]
+        [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(Object), Description ="Message to Send")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(object), Description = "The OK response")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "POST", Route = "messages")] HttpRequest req)
@@ -47,21 +46,24 @@ namespace Toast.Sms.Triggers
             var baseUrl = Environment.GetEnvironmentVariable("Toast__BaseUrl");
             var version = Environment.GetEnvironmentVariable("Toast__Version");
             var endpoint = Environment.GetEnvironmentVariable("Toast__Endpoints__SendMessages");
-            var body = req.Query["body"].ToString();
-            var sendNo = req.Query["sendNo"].ToString();
-            var recipientNo = req.Query["recipientNo"].ToString();
             var options = new
             {
                 version = version,
                 appKey = appKey
             };
 
-            var json = $"{{\"body\":\"{body}\",\"sendNo\":\"{sendNo}\",\"recipientList\":[{{\"recipientNo\":\"{recipientNo}\"}}]}}";
+            var data = default(object);
+            using (var reader  = new StreamReader(req.Body))
+            {
+                var json = await reader.ReadToEndAsync().ConfigureAwait(false);
+                data = JsonConvert.DeserializeObject<object>(json);
+            }
+
             var requestUrl = Smart.Format($"{baseUrl.TrimEnd('/')}/{endpoint.TrimStart('/')}", options);
 
             var http = new HttpClient();
 
-            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            var content = new ObjectContent<object>(data, new JsonMediaTypeFormatter(), "application/json");
 
             // Act
             http.DefaultRequestHeaders.Add("X-Secret-Key", secretKey);
