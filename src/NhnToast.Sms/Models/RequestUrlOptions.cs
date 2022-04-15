0ed763fb3ceb127980c51f5ad5764b38d96e1f4c
dunblx05 <dubblx05@naver.com>
2022-04-15 19:29:42 +0900

    'public class' to 'public abstract class'

--- src/NhnToast.Sms/Models/RequestUrlOptions.cs
+++ src/NhnToast.Sms/Models/RequestUrlOptions.cs
@@ -1,5 +1,5 @@
-public class RequestUrlOptions
+public abstract class RequestUrlOptions
 {
-    public virtual string version{ get; set; }
-    public virtual string appkey{get; set;}
+    public virtual string version { get; set; }
+    public virtual string appkey { get; set; }
 }
