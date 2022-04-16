162112c585bb9cee7cec667e23e098bf7ed71b71
Justin Yoo <justin.yoojh@gmail.com>
2022-04-16 11:44:20 +0900

    Update smartformatter to avoid case sensitivity

--- src/NhnToast.Sms/Startup.cs
+++ src/NhnToast.Sms/Startup.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Configurations.AppSettings.Extensions;
 using Microsoft.Extensions.Configuration;
@@ -11,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Toast.Sms.Configurations;
 
 [assembly: FunctionsStartup(typeof(Toast.Sms.Startup))]
+
 namespace Toast.Sms
 {
     public class Startup : FunctionsStartup
