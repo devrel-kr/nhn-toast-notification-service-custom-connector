d9bb5b0971ce437197f16ea56eec0e7466338d33
Justin Yoo <justin.yoojh@gmail.com>
2022-05-03 20:04:35 +0900

    Add request header validator

---
+++ src/NhnToast.Common/Models/RequestHeaderModel.cs
@@ -0,0 +1,23 @@
+using Newtonsoft.Json;
+
+namespace Toast.Common.Models
+{
+    /// <summary>
+    /// This represents the model entity for request header.
+    /// </summary>
+    public class RequestHeaderModel
+    {
+        /// <summary>
+        /// Gets or sets the app key header.
+        /// </summary>
+        [JsonProperty("x-app-key")]
+        public virtual string AppKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the secret key header.
+        /// </summary>
+        [JsonProperty("x-secret-key")]
+        public virtual string SecretKey { get; set; }
+    }
+
+}
