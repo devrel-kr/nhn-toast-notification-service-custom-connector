9d24ff4f7eb33bada6dd64d6ff08eca6d8f32caa
Park jimin <wlals761@naver.com>
2022-05-08 19:21:02 +0900

    Update setup-apim.sh
    파라미터로 함수앱 이름 받아오게 수정

--- infra/setup-apim.sh
+++ infra/setup-apim.sh
@@ -7,10 +7,8 @@ names=("SmsApp" "SmsVerificationApp")
 #'dev', 'test', 'prod', 'kdy', 'kms', 'lsw', 'pjm'
 environment=$1
 
-directoryIndex=4
-
-cd ../src
-directoryList=($(echo `ls`))
+fncappSuffixes=($2 $3)
+suffixIndex=0
 
 version=$(curl -H "Accept: application/vnd.github.v3+json" \
 https://api.github.com/repos/devrel-kr/nhn-toast-notification-service-custom-connector/releases/latest| \
@@ -24,8 +22,9 @@ for name in ${names[@]}
 do
     filename="$name-$version.zip"
     smszip=$(echo $urls | jq --arg v $filename 'select(.name == $v) | .url' -r)
-    fncappName="fncapp-${directoryList[$directoryIndex]}-$environment-krc"
-    directoryIndex=`expr $directoryIndex + 1`
+    fncappName="fncapp-${fncappSuffixes[$suffixIndex]}-$environment-krc"
+    suffixIndex=`expr $suffixIndex + 1`
+    echo $fncappName
     fncappUrl="https://$fncappName.azurewebsites.net/api/openapi/v3.json"
     smsapp=$(az functionapp deploy -g rg-nt-$environment-krc -n $fncappName --src-url $smszip --type zip)
     az deployment group create -n ApiManagement_Api-$name -g rg-nt-$environment-krc -u https://raw.githubusercontent.com/devrel-kr/nhn-toast-notification-service-custom-connector/main/infra/provision-apiManagementApi.json -p name=nt -p env=$environment -p apiMgmtNameValueName=$fncappName -p apiMgmtNameValueDisplayName=$fncappName -p apiMgmtNameValueValue=$fncappName -p apiMgmtApiName=$fncappName -p apiMgmtApiDisplayName=$fncappName -p apiMgmtApiDescription=$fncappName -p apiMgmtApiPath=$fncappName -p apiMgmtApiValue=$fncappUrl
