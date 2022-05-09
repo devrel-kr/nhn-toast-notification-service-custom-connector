e21b4ebc2246eed613a7fb43150cbc3da55a2cb7
Park jimin <wlals761@naver.com>
2022-05-09 22:50:02 +0900

    Update setup-apim.sh
    suffixes 값을 ','로 구분하여 배열처리
    AZ_ENVIRONMENT_CODE, AZ_APP_SUFFIXES, AZ_LOCATION_CODE 값 외부에서 환경변수로 주입한다고 가정

--- infra/setup-apim.sh
+++ infra/setup-apim.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
-set -e
+declare -a fncappSuffixes
+Suffixindex=1
 
-names=("SmsApp" "SmsVerificationApp")
-
-#'dev', 'test', 'prod', 'kdy', 'kms', 'lsw', 'pjm'
-environment=$1
-
-fncappSuffixes=($2 $3)
-suffixIndex=0
-
-version=$(curl -H "Accept: application/vnd.github.v3+json" \
-https://api.github.com/repos/devrel-kr/nhn-toast-notification-service-custom-connector/releases/latest| \
-jq '.tag_name' -r)
+# ','기준으로 나누기
+while [ "$(echo $AZ_APP_SUFFIXES | cut -d ',' -f $Suffixindex)" != ""  ]
+do
+    fncappSuffixes[$Suffixindex-1]=$(echo $AZ_APP_SUFFIXES | cut -d ',' -f $Suffixindex)
+    Suffixindex=`expr $Suffixindex + 1`
+done
+Suffixindex=`expr $Suffixindex - 2`
 
 urls=$(curl -H "Accept: application/vnd.github.v3+json" \
 https://api.github.com/repos/devrel-kr/nhn-toast-notification-service-custom-connector/releases/latest| \
-jq '.assets[] | { name: .name, url: .browser_download_url }')
+jq '[.assets[] | .browser_download_url]')
 
-for name in ${names[@]}
+for value in `eval echo {0..$Suffixindex}`
 do
-    filename="$name-$version.zip"
-    smszip=$(echo $urls | jq --arg v $filename 'select(.name == $v) | .url' -r)
-    fncappName="fncapp-${fncappSuffixes[$suffixIndex]}-$environment-krc"
-    suffixIndex=`expr $suffixIndex + 1`
+    smszip=$(echo $urls | jq --argjson value $value '.[$value]' -r)
+    fncappName="fncapp-${fncappSuffixes[$value]}-$AZ_ENVIRONMENT_CODE-$AZ_LOCATION_CODE"
     fncappUrl="https://$fncappName.azurewebsites.net/api/openapi/v3.json"
-    smsapp=$(az functionapp deploy -g rg-nt-$environment-krc -n $fncappName --src-url $smszip --type zip)
-    az deployment group create -n ApiManagement_Api-$name -g rg-nt-$environment-krc -u https://raw.githubusercontent.com/devrel-kr/nhn-toast-notification-service-custom-connector/main/infra/provision-apiManagementApi.json -p name=nt -p env=$environment -p apiMgmtNameValueName=$fncappName -p apiMgmtNameValueDisplayName=$fncappName -p apiMgmtNameValueValue=$fncappName -p apiMgmtApiName=$fncappName -p apiMgmtApiDisplayName=$fncappName -p apiMgmtApiDescription=$fncappName -p apiMgmtApiPath=$fncappName -p apiMgmtApiValue=$fncappUrl
+    smsapp=$(az functionapp deploy -g rg-nt-$AZ_ENVIRONMENT_CODE-$AZ_LOCATION_CODE -n $fncappName --src-url $smszip --type zip)
+    az deployment group create -n ApiManagement_Api-${fncappSuffixes[$value]} -g rg-nt-$AZ_ENVIRONMENT_CODE-$AZ_LOCATION_CODE -u https://raw.githubusercontent.com/devrel-kr/nhn-toast-notification-service-custom-connector/main/infra/provision-apiManagementApi.json -p name=nt -p env=$AZ_ENVIRONMENT_CODE -p apiMgmtNameValueName=$fncappName -p apiMgmtNameValueDisplayName=$fncappName -p apiMgmtNameValueValue=$fncappName -p apiMgmtApiName=$fncappName -p apiMgmtApiDisplayName=$fncappName -p apiMgmtApiDescription=$fncappName -p apiMgmtApiPath=$fncappName -p apiMgmtApiValue=$fncappUrl
 done
