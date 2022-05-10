8c13f7fdf658bd485478672757f95259f1ff9d54
dunblx05 <dubblx05@naver.com>
2022-05-10 20:41:44 +0900

    Resolve conflict

--- src/nt-sms/Models/ListMessageStatusRequestQuries.cs
+++ src/nt-sms/Models/ListMessageStatusRequestQuries.cs
@@ -10,19 +10,16 @@ namespace Toast.Sms.Models
         /// <summary>
         /// Gets or sets the start update date.
         /// </summary>
-        [JsonProperty("startUpdateDate")]
         public virtual string StartUpdateDate { get; set; }
 
         /// <summary>
         /// Gets or sets the end update date.
         /// </summary>
-        [JsonProperty("endUpdateDate")]
         public virtual string EndUpdateDate { get; set; }
 
         /// <summary>
         /// Gets or sets the message type.
         /// </summary>
-        [JsonProperty("messageType")]
         public virtual string MessageType { get; set; }
         /// <summary>
         /// Gets or sets the page number.
--- src/nt-sms/Models/ListMessagesRequestQueries.cs
+++ src/nt-sms/Models/ListMessagesRequestQueries.cs
@@ -10,43 +10,38 @@ namespace Toast.Sms.Models
         /// <summary>
         /// Gets or sets the request id.
         /// </summary>
-        [JsonProperty("requestId")]
-        public virtual string RequestIden { get; set; }
+        public virtual string RequestId { get; set; }
 
         /// <summary>
         /// Gets or sets the start request date.
         /// </summary>
-        [JsonProperty("startRequestDate")]
-        public virtual string StartReqDate { get; set; }
+        public virtual string StartRequestDate { get; set; }
 
         /// <summary>
         /// Gets or sets the end request date.
         /// </summary>
-        [JsonProperty("endRequestDate")]
-        public virtual string EndReqDate { get; set; }
+        public virtual string EndRequestDate { get; set; }
 
         /// <summary>
         /// Gets or sets the start create date.
         /// </summary>
         [JsonProperty("startCreateDate")]
-        public virtual string StartCreDate { get; set; }
+        public virtual string StartCreateDate { get; set; }
 
         /// <summary>
         /// Gets or sets the end create date.
         /// </summary>
         [JsonProperty("endCreateDate")]
-        public virtual string EndCreDate { get; set; }
+        public virtual string EndCreateDate { get; set; }
 
         /// <summary>
         /// Gets or sets the start result date.
         /// </summary>
-        [JsonProperty("startResultDate")]
         public virtual string StartResultDate { get; set; }
 
         /// <summary>
         /// Gets or sets the end result date.
         /// </summary>
-        [JsonProperty("endResultDate")]
         public virtual string EndResultDate { get; set; }
 
         /// <summary>
@@ -64,37 +59,31 @@ namespace Toast.Sms.Models
         /// <summary>
         /// Gets or sets the template id.
         /// </summary>
-        [JsonProperty("templateId")]
         public virtual string TemplateId { get; set; }
 
         /// <summary>
         /// Gets or sets the message status.
         /// </summary>
-        [JsonProperty("msgStatus")]
         public virtual string MessageStatus { get; set; }
 
         /// <summary>
         /// Gets or sets the reusult code.
         /// </summary>
-        [JsonProperty("resultCode")]
         public virtual string ResultCode { get; set; }
 
         /// <summary>
         /// Gets or sets the sub result code.
         /// </summary>
-        [JsonProperty("subResultCode")]
         public virtual string SubResultCode { get; set; }
 
         /// <summary>
         /// Gets or sets the sender grouping key.
         /// </summary>
-        [JsonProperty("senderGroupingKey")]
         public virtual string SenderGroupingKey { get; set; }
 
         /// <summary>
         /// Gets or sets the recipient grouping key.
         /// </summary>
-        [JsonProperty("recipientGroupingKey")]
         public virtual string RecipientGroupingKey { get; set; }
 
         /// <summary>
