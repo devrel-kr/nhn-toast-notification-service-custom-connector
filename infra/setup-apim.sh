4f90f5e11ea26fa03be37f2b0fdc77a03903108f
Park jimin <wlals761@naver.com>
2022-05-10 21:27:09 +0900

    Update setup-apim.sh
    파라미터 값 수정
    펑션 앱 세팅 업데이트 추가

--- infra/setup-apim.sh
+++ infra/setup-apim.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+bicepUrl="https://raw.githubusercontent.com/devrel-kr/nhn-toast-notification-service-custom-connector/main/infra/provision-apiManagementApi.json"
+
+resourceGroup="rg-$AZ_RESOURCE_NAME-$AZ_ENVIRONMENT_CODE-$AZ_LOCATION_CODE"
+
 declare -a fncappSuffixes
 Suffixindex=1
 
@@ -17,9 +21,44 @@ jq '[.assets[] | .browser_download_url]')
 
 for value in `eval echo {0..$Suffixindex}`
 do
-    smszip=$(echo $urls | jq --argjson value $value '.[$value]' -r)
-    fncappName="fncapp-${fncappSuffixes[$value]}-$AZ_ENVIRONMENT_CODE-$AZ_LOCATION_CODE"
+    apizip=$(echo $urls | jq --argjson value $value '.[$value]' -r)
+    fncappName="fncapp-$AZ_RESOURCE_NAME-${fncappSuffixes[$value]}-$AZ_ENVIRONMENT_CODE-$AZ_LOCATION_CODE"
     fncappUrl="https://$fncappName.azurewebsites.net/api/openapi/v3.json"
-    smsapp=$(az functionapp deploy -g rg-nt-$AZ_ENVIRONMENT_CODE-$AZ_LOCATION_CODE -n $fncappName --src-url $smszip --type zip)
-    az deployment group create -n ApiManagement_Api-${fncappSuffixes[$value]} -g rg-nt-$AZ_ENVIRONMENT_CODE-$AZ_LOCATION_CODE -u https://raw.githubusercontent.com/devrel-kr/nhn-toast-notification-service-custom-connector/main/infra/provision-apiManagementApi.json -p name=nt -p env=$AZ_ENVIRONMENT_CODE -p apiMgmtNameValueName=$fncappName -p apiMgmtNameValueDisplayName=$fncappName -p apiMgmtNameValueValue=$fncappName -p apiMgmtApiName=$fncappName -p apiMgmtApiDisplayName=$fncappName -p apiMgmtApiDescription=$fncappName -p apiMgmtApiPath=$fncappName -p apiMgmtApiValue=$fncappUrl
+
+    # Deploy function apps
+    apiapp=$(az functionapp deploy -g $resourceGroup -n $fncappName --src-url $apizip --type zip)
+
+    apiKey=$(az functionapp keys list -g $resourceGroup -n $fncappName --query "functionKeys.default" -o tsv)
+    apiPath=$(echo ${fncappSuffixes[$value]} | tr '-' '/')
+    apiValue=$(echo ${fncappSuffixes[$value]} | tr '[a-z]' '[A-Z]')
+    apiValue=$(echo $apiValue | tr '-' '_')
+    apiValue="X_FUNCTIONS_KEY_$apiValue"
+
+    # Provision APIs to APIM
+    az deployment group create \
+    -n ApiManagement_Api-${fncappSuffixes[$value]} \
+    -g $resourceGroup \
+    -u $bicepUrl \
+    -p name=$AZ_RESOURCE_NAME \
+    -p env=$AZ_ENVIRONMENT_CODE \
+    -p apiMgmtNameValueName=$apiValue \
+    -p apiMgmtNameValueDisplayName=$apiValue \
+    -p apiMgmtNameValueValue=$apiKey \
+    -p apiMgmtApiName=$apiValue \
+    -p apiMgmtApiDisplayName=$apiValue \
+    -p apiMgmtApiDescription=$apiValue \
+    -p apiMgmtApiPath=$apiPath \
+    -p apiMgmtApiValue=$fncappUrl
+
+    apiUrl="https://apim-$AZ_RESOURCE_NAME-$AZ_ENVIRONMENT_CODE-$AZ_LOCATION_CODE.azure-api.net/$apiPath"
+    settingList=$(az functionapp config appsettings list -g $resourceGroup -n $fncappName | jq '.[] | select(.name == "OpenApi__HostNames") | .value' -r)
+    if [ "$settingList" == "" ]
+    then
+        apihostnames=$apiUrl
+    else
+        apihostnames=$apiUrl,$settingList
+    fi
+
+    # Update app settings on function apps
+    apisettings=$(az functionapp config appsettings set -g $resourceGroup -n $fncappName --settings OpenApi__HostNames=$apihostnames)
 done
