8006caac0bb0881ecc0a135accaa4950142beea0
dunblx05 <dubblx05@naver.com>
2022-05-17 19:07:16 +0900

    Add Exceptions

--- src/nt-common/Exceptions/RequestQueryNotValidException.cs
+++ src/nt-common/Exceptions/RequestQueryNotValidException.cs
@@ -5,42 +5,42 @@ using System.Runtime.Serialization;
 namespace Toast.Common.Exceptions
 {
     /// <summary>
-    /// This represents the exception entity for RequestHeaderNotValidException.
+    /// This represents the exception entity for RequestQueryNotValidException.
     /// </summary>
-    public class RequestHeaderNotValidException : ToastException
+    public class RequestQueryNotValidException : ToastException
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="RequestHeaderNotValidException"/> class.
+        /// Initializes a new instance of the <see cref="RequestQueryNotValidException"/> class.
         /// </summary>
-        public RequestHeaderNotValidException()
+        public RequestQueryNotValidException()
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="RequestHeaderNotValidException"/> class.
+        /// Initializes a new instance of the <see cref="RequestQueryNotValidException"/> class.
         /// </summary>
         /// <param name="message">Exception message.</param>
-        public RequestHeaderNotValidException(string message)
+        public RequestQueryNotValidException(string message)
             : base(message)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="RequestHeaderNotValidException"/> class.
+        /// Initializes a new instance of the <see cref="RequestQueryNotValidException"/> class.
         /// </summary>
         /// <param name="message">Exception message.</param>
         /// <param name="innerException"><see cref="Exception"/> instance as the inner exception.</param>
-        public RequestHeaderNotValidException(string message, Exception innerException)
+        public RequestQueryNotValidException(string message, Exception innerException)
             : base(message, innerException)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="RequestHeaderNotValidException"/> class.
+        /// Initializes a new instance of the <see cref="RequestQueryNotValidException"/> class.
         /// </summary>
         /// <param name="info"><see cref="SerializationInfo"/> instance.</param>
         /// <param name="context"><see cref="StreamingContext"/> instance.</param>
-        protected RequestHeaderNotValidException(SerializationInfo info, StreamingContext context)
+        protected RequestQueryNotValidException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
         }
