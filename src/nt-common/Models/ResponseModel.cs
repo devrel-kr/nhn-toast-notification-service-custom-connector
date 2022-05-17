47b4a60a2f3a531e8338fd762da476a6ffdc8465
Justin Yoo <justin.yoojh@gmail.com>
2022-05-17 15:02:07 +0000

    Update test cases for common response models (#53)

--- src/nt-common/Models/ResponseModel.cs
+++ src/nt-common/Models/ResponseModel.cs
@@ -1,24 +1,18 @@
-using Newtonsoft.Json;
-
 namespace Toast.Common.Models
 {
     /// <summary>
     /// This represents the model entity for response.
     /// </summary>
-    public abstract class ResponseModel<T> 
+    public abstract class ResponseModel<T>
     {
         /// <summary>
         /// Gets or sets the header.
         /// </summary>
-        [JsonProperty("header")]
         public virtual ResponseHeaderModel Header { get; set; } = new ResponseHeaderModel();
 
         /// <summary>
         /// Gets or sets the body.
         /// </summary>
-        [JsonProperty("body")]
         public virtual T Body { get; set; }
-
     }
-
 }
