53a3909801d628d1bf2ca03956a9f3c0c85cf778
Justin Yoo <justin.yoojh@gmail.com>
2022-05-30 19:35:39 +0900

    Fix typo

--- src/nt-sms/Startup.cs
+++ src/nt-sms/Startup.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Configurations.AppSettings.Extensions;
 using Microsoft.Extensions.Configuration;
@@ -5,6 +6,8 @@ using Microsoft.Extensions.DependencyInjection;
 
 using Toast.Common.Configurations;
 using Toast.Sms.Configurations;
+using Toast.Sms.Models;
+using Toast.Sms.Validators;
 
 [assembly: FunctionsStartup(typeof(Toast.Sms.Startup))]
 
@@ -24,6 +27,7 @@ namespace Toast.Sms
         {
             ConfigureAppSettings(builder.Services);
             ConfigureHttpClient(builder.Services);
+            ConfigureValidators(builder.Services);
         }
 
         private static void ConfigureAppSettings(IServiceCollection services)
@@ -37,6 +41,14 @@ namespace Toast.Sms
         private static void ConfigureHttpClient(IServiceCollection services)
         {
             services.AddHttpClient("messages");
+        }
+
+        private static void ConfigureValidators(IServiceCollection services)
+        {
+            services.AddScoped<IValidator<GetMessageRequestQueries>, GetMessageRequestQueryValidator>();
+            services.AddScoped<IValidator<ListMessagesRequestQueries>, ListMessagesRequestQueryValidator>();
+            services.AddScoped<IValidator<ListMessageStatusRequestQueries>, ListMessageStatusRequestQueryValidator>();
+            services.AddScoped<IValidator<SendMessagesRequestBody>, SendMessagesRequestBodyValidator>();
         }
     }
 }
