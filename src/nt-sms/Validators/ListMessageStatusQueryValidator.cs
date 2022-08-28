77dc916a9dd6930642de8fa434ac3ad7dac0666d
SangminPark <psmdc0714@gmail.com>
2022-08-29 00:25:49 +0900

    Edit Field Convention of Validators
    Edit Field Convention of Validators and Simplify if block in Wrapper

--- src/nt-sms/Validators/ListMessageStatusQueryValidator.cs
+++ src/nt-sms/Validators/ListMessageStatusQueryValidator.cs
@@ -64,7 +64,7 @@ namespace Toast.Sms.Validators
 
         private bool IsValidDateFormat(string date)
         {
-            return _regex.IsMatch(date);
+            return this._regex.IsMatch(date);
         }
 
         List<string> MsgType = new List<string>() { "SMS", "LMS", "MMS", "AUTH" };
